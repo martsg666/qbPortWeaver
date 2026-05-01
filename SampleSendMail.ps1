88d223ab7e74aabf98e6c86a3533b710bf03ba26
martsg666 <martsg@gmail.com>
2026-05-01 15:16:21 -0400

    Switch mail scripts to HTML body for reliable column alignment

--- SampleSendMail.ps1
+++ SampleSendMail.ps1
@@ -7,24 +7,29 @@ $SmtpPort = 587
 $From = "FROM_EMAIL@gmail.com"
 $To = "TO_EMAIL@gmail.com"
 $Subject = "qbPortWeaver - Port Changed to $($status.clientPort)"
+function Row($key, $val) { "<tr><td style='padding:1px 16px 1px 0;color:#888;'>$key</td><td>$val</td></tr>" }
+function Section($title) { "<tr><td colspan='2' style='padding:10px 0 2px;font-weight:bold;'>$title</td></tr>" }
+
 $Body = @"
-[App]
-  Version          : $($status.appVersion)
-  Timestamp        : $($status.timestamp)
-  Update Interval  : $($status.updateIntervalSeconds)s
-[VPN]
-  Provider         : $($status.vpnProvider)
-  Connected        : $($status.vpnConnected)
-  Port             : $($status.vpnPort)
-[Client]
-  Name             : $($status.client)
-  Running          : $($status.clientRunning)
-  Port             : $($status.clientPort)
-  Previous Port    : $($status.clientPreviousPort)
-  Port Changed     : $($status.portChanged)
-[Result]
-  Status           : $($status.status)
-  Message          : $($status.message)
+<table style='font-family:monospace;font-size:13px;border-collapse:collapse;'>
+$(Section '[App]')
+$(Row 'Version'         $status.appVersion)
+$(Row 'Timestamp'       $status.timestamp)
+$(Row 'Update Interval' "$($status.updateIntervalSeconds)s")
+$(Section '[VPN]')
+$(Row 'Provider'  $status.vpnProvider)
+$(Row 'Connected' $status.vpnConnected)
+$(Row 'Port'      $status.vpnPort)
+$(Section '[Client]')
+$(Row 'Name'          $status.client)
+$(Row 'Running'       $status.clientRunning)
+$(Row 'Port'          $status.clientPort)
+$(Row 'Previous Port' $status.clientPreviousPort)
+$(Row 'Port Changed'  $status.portChanged)
+$(Section '[Result]')
+$(Row 'Status'  $status.status)
+$(Row 'Message' $status.message)
+</table>
 "@
 
 # Credentials
@@ -37,6 +42,7 @@ Send-MailMessage -From $From `
                  -To $To `
                  -Subject $Subject `
                  -Body $Body `
+                 -BodyAsHtml `
                  -SmtpServer $SmtpServer `
                  -Port $SmtpPort `
                  -UseSsl `
